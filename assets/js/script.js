cf2120c93dc9cfcafea02bd1dddfdd5e487d8ae2
Ragnarwof94 <rodolfo.guerrero.cisterna@gmail.com>
2024-01-27 01:19:27 -0300

    se cambia nombre al archivo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 window.onscroll = function() {
     var navbar = document.querySelector('.navbar');
     if (window.pageYOffset > 0) {
-      navbar.style.backgroundColor = '#007B9E'; 
+      navbar.style.backgroundColor = '#0DCAF0'; 
     } else {
       navbar.style.backgroundColor = 'transparent' ;
     }
   };
-  
+
   //Smooth Scroll//
   $(document).ready(function(){
     $("a").on('click', function(event) {
