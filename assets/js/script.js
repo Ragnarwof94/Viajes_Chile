2ac89d4ae499104268f297295bd97ae969aeb3de
Ragnarwof94 <rodolfo.guerrero.cisterna@gmail.com>
2024-02-05 15:44:55 -0300

    Se corrige efecto blur cuando la página está al inicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,42 +19,46 @@ $(document).ready(function () {
       let navegador = $(".navbar");
       if ($(this).scrollTop() > 100) {
         navegador.css({
-          "background": "rgba(13, 202, 240, 0.5)",
-          "backdrop-filter": "blur(10px)" // Agrega desenfoque
+          background: "rgba(13, 202, 240, 0.5)",
+          "backdrop-filter": "blur(10px)", // Agrega desenfoque
         });
       } else {
-        navegador.css("background", "none");
+        navegador.css({
+          background: "none",
+          "backdrop-filter": "none", // Remueve el desenfoque
+        });
       }
     });
   });
 });
 
-
 //Nueva instancia del carrusel con la opción pause establecida en false
 // El carrusel no se pausará cuando el mouse esté encima.
-let carousel = new bootstrap.Carousel(document.querySelector('#carouselExample'), {
-  pause: false
-});
+let carousel = new bootstrap.Carousel(
+  document.querySelector("#carouselExample"),
+  {
+    pause: false,
+  }
+);
 
 //Event Listener botón enviar//
-document.addEventListener('DOMContentLoaded', function() {
-  let form = document.querySelector('#Contacto form');
-  form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      alert('Su mensaje ha sido enviado correctamente');
+document.addEventListener("DOMContentLoaded", function () {
+  let form = document.querySelector("#Contacto form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Su mensaje ha sido enviado correctamente");
   });
 });
 
 //Tooltip de ayuda botón enviar
-document.addEventListener('DOMContentLoaded', function() {
-  let button = document.querySelector('#Contacto button');
-  button.setAttribute('data-toggle', 'tooltip');
-  button.setAttribute('data-placement', 'right');
-  button.setAttribute('title', 'Haz clic para enviar tu mensaje');
+document.addEventListener("DOMContentLoaded", function () {
+  let button = document.querySelector("#Contacto button");
+  button.setAttribute("data-toggle", "tooltip");
+  button.setAttribute("data-placement", "right");
+  button.setAttribute("title", "Haz clic para enviar tu mensaje");
 
   // Inicializar todos los tooltips en el documento
   $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
   });
 });
-
